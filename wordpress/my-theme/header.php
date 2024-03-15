3485a7e9fcd4ba70ae96630a1cbd706d4647b98b
Oriol Testart <oriol.testart@6tems.com>
2024-03-15 23:36:12 +0100

    Empty theme

--- wordpress/my-theme/header.php
+++ wordpress/my-theme/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html "<?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
-    header
+
+<body class="<?= body_class() ?>">
