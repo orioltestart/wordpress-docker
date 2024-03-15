3483e065c59a8a26145077775b40b6d05159da57
Oriol Testart <oriol.testart@6tems.com>
2024-03-15 23:24:13 +0100

    First theme configuration

---
+++ wordpress/my-theme/functions.php
@@ -0,0 +1,90 @@
+<?php
+// Desactivar wpemoji
+add_action('init', function () {
+    // Deshabilitar wpemoji
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+    remove_action('wp_head', 'feed_links', 2); // Feed links
+    remove_action('wp_head', 'feed_links_extra', 3); // Category feed links
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'rsd_link');
+
+    remove_action('wp_head', 'print_embed_styles');
+    remove_action('wp_head', 'print_oembed_styles');
+    remove_action('wp_print_styles', 'print_media_styles');
+    remove_action('wp_print_styles', 'print_admin_styles');
+    remove_action('wp_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_head', 'wp_print_styles');
+
+    // Quitar el enlace al endpoint api.w.org
+    remove_action('wp_head', 'rest_output_link_wp_head', 10);
+    // Quitar el estilo del editor clásico    
+    remove_action('wp_head', 'classic_editor_add_customizer_css');
+
+    remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+    remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
+
+    // Quitar los scripts emoji desde el editor de texto
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+
+});
+
+// Quitar los estilos del administrador cuando no se está autenticado
+function remove_admin_styles_for_non_logged_users()
+{
+    if (!is_user_logged_in()) {
+        // Desregistrarse de los estilos del administrador
+        wp_deregister_style('wp-admin');
+        wp_deregister_style('buttons');
+        wp_deregister_style('wp-auth-check');
+        wp_deregister_style('wp-color-picker');
+        wp_deregister_style('admin-bar');
+        wp_deregister_style('dashicons');
+        wp_deregister_style('editor-buttons');
+        wp_deregister_style('media-views');
+        wp_deregister_style('wp-jquery-ui-dialog');
+
+        // Desregistrarse de los estilos de los bloques de Gutenberg
+        wp_deregister_style('wp-block-library');
+        wp_deregister_style('wp-block-library-theme');
+        
+        wp_dequeue_style('classic-theme-styles');
+    }
+}
+add_action('wp_enqueue_scripts', 'remove_admin_styles_for_non_logged_users', 999);
+
+
+// Desactivar XML-RPC
+add_filter('xmlrpc_enabled', '__return_false');
+
+
+// Desactivar los feeds RSS2
+function disable_rss2_feed()
+{
+    wp_die('Los feeds RSS2 están desactivados en este sitio.');
+}
+add_action('do_feed_rss2', 'disable_rss2_feed', 1);
+
+
+function disable_emojis_tinymce($plugins)
+{
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    } else {
+        return array();
+    }
+}
+
+
+// Deactivate jQuery
+function deactivate_jquery() {
+    wp_dequeue_script('jquery');
+    wp_deregister_script('jquery');
+}
+add_action('wp_enqueue_scripts', 'deactivate_jquery');
---
+++ wordpress/my-theme/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Este es el archivo de plantilla predeterminado de tu tema de WordPress.
+ * Se utiliza para mostrar una página de inicio si no se ha configurado una página estática.
+ */
+
+get_header(); // Cargar la cabecera del tema
+
+// Contenido del índice aquí
+
+get_footer(); // Cargar el pie de página del tema
